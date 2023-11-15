adce62981192af997748cf8b757f7370c0f64b5d
facekak <facekak5060@gmail.com>
2023-11-15 10:03:20 +0700

    Gamemanager and ball script and mat physics

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.PlayerLoop;
+
+public enum BallColor
+{
+      White,
+      Red,
+      Yellow,
+      Green,
+      Brown,
+      Blue,
+      Pink,
+      Black
+}
+public class Ball : MonoBehaviour
+{
+    
+    [SerializeField] private int point;
+    public int Point { get { return point; } }
+
+    [SerializeField] private BallColor ballColor;
+
+    [SerializeField] private MeshRenderer rd;
+    private void Awake()
+    {
+        rd = GetComponent<MeshRenderer>();
+    }
+
+    public void SetColorAndPoint(BallColor col)
+    {
+        switch (col)
+        {
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int playerScore;
+
+    public int PlayerScore
+    {
+        get { return playerScore; }
+        set { playerScore = value; }
+    }
+
+    [SerializeField] private GameObject ballPrefab;
+
+    [SerializeField] private GameObject[] ballPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
