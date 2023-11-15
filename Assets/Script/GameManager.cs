34253b2c47ab45d1747deca10d2441323795d3e6
facekak <facekak5060@gmail.com>
2023-11-15 10:17:52 +0700

    Spawn red ball

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -16,15 +16,27 @@ public class GameManager : MonoBehaviour
 
     [SerializeField] private GameObject[] ballPosition;
 
+    public static GameManager instance;
+
     // Start is called before the first frame update
     void Start()
     {
+        instance = this;
         
+        SetBall(BallColor.Red,1);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void SetBall(BallColor col, int i)
+    {
+        GameObject obj = Instantiate(ballPrefab, ballPosition[i].transform.position, Quaternion.identity);
+
+        Ball b = obj.GetComponent<Ball>();
+        b.SetColorAndPoint(col);
     }
 }
