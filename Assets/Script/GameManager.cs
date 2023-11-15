9aab5b76b9714104849474c171acab91a05af494
facekak <facekak5060@gmail.com>
2023-11-15 10:24:15 +0700

    Spawn all balls and colors

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -23,7 +23,14 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
         
+        SetBall(BallColor.White,0);
         SetBall(BallColor.Red,1);
+        SetBall(BallColor.Yellow,2);
+        SetBall(BallColor.Green,3);
+        SetBall(BallColor.Brown,4);
+        SetBall(BallColor.Blue,5);
+        SetBall(BallColor.Pink,6);
+        SetBall(BallColor.Black,7);
     }
 
     // Update is called once per frame
