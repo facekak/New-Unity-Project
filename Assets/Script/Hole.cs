26810907d173fb77ef362b3dd86546ce049e30b0
facekak <facekak5060@gmail.com>
2023-11-25 17:31:37 +0700

    Hole bug / no UI sore

---
+++ Assets/Script/Hole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        Ball b = collision.gameObject.GetComponent<Ball>();
+
+        if (b != null)
+        {
+            GameManager.instance.PlayerScore += b.Point;
+            Destroy(b.gameObject);
+        }
+    }
+}
